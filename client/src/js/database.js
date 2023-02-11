0d0fb23118c40d09d7d8de829db9346148939199
Jwarren619 <jjeremiahwarren@yahoo.com>
2023-02-11 11:21:43 -0600

    added my json

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const getDb = async (content) => {
+export const putDb = async (content) => {
    console.error('putDb not implemented');
 
    const jateDb = await openDB('jate', 1);
@@ -33,7 +33,7 @@ export const getDb = async (content) => {
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getAllDb = async (content) => {
+export const getDb = async (content) => {
 console.log('GET all the content from the database', id);
 
 const jateDb = await openDB('jate', 1);
